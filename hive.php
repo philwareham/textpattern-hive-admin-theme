88f2bff7ff8157317d8a4c69831b1ad735e969ec
Phil Wareham <phil@designhive.com>
2012-11-03 17:26:44 +0000

    Couple of newlines for code neatness

--- hive.php
+++ hive.php
@@ -59,7 +59,7 @@ class hive_theme extends theme
 		$out[] = '<script src="modernizr.js"></script>';
 		$out[] = '<script src="'.$this->url.'js/tinynav.min.js"></script>';
 		$out[] = '<script src="'.$this->url.'js/scripts.js"></script>';
-		$out[] = '<!--[if lt IE 9]><script src="'.$this->url.'js/selectivizr.min.js"></script><![endif]-->';
+		$out[] = '<!--[if lt IE 9]><script src="'.$this->url.'js/selectivizr.min.js"></script><![endif]-->'.n;
 
 		return join(n, $out);
 	}
@@ -100,7 +100,7 @@ class hive_theme extends theme
 
 	function footer()
 	{
-		$out[] = '<p class="mothership"><a href="http://textpattern.com" title="'.gTxt('go_txp_com').'" rel="external">Textpattern CMS</a> (v'.txp_version.')</p>';
+		$out[] = n.'<p class="mothership"><a href="http://textpattern.com" title="'.gTxt('go_txp_com').'" rel="external">Textpattern CMS</a> (v'.txp_version.')</p>';
 		$out[] = '<p class="pagejump"><a href="#">'.gTxt('back_to_top').'</a></p>';
 		return join(n, $out);
 	}
