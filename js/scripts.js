4b3b6c8dd444d15a1f907721e2380a9bd17c6afd
Phil Wareham <phil@designhive.com>
2012-11-08 16:04:22 +0000

    Consistency in JavaScript code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,14 @@
-jQuery(function () {
-
-
+jQuery(function ()
+{
 	// To make select navigation menu work (via TinyNav)
 	$('.untinynav').tinyNav({
 		active: 'selected'
 	});
 
-
 	// External links open new window (target="_blank" replacement)
-	$('[rel="external"]').click( function() {
-		window.open( $(this).attr('href') );
+	$('[rel="external"]').click(function ()
+	{
+		window.open($(this).attr('href'));
 		return false;
 	});
-
-
 });
