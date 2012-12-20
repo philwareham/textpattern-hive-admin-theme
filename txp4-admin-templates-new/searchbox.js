e754b8745a7533d23ba4dea42e7935a1d8626af6
Phil Wareham <phil@designhive.com>
2012-12-20 18:13:13 +0000

    Non-core: WIP of search functionality

--- txp4-admin-templates-new/searchbox.js
+++ txp4-admin-templates-new/searchbox.js
@@ -27,3 +27,36 @@ $(function() {
 			.attr('disabled', this.checked);
 	});
 });
+
+
+
+$(function() {
+	var $ui = $('.txp-search');
+
+	// On focus and on click, display the search options and change the arrow image.
+	$ui.find('[type="search"]').bind('focus click', function()
+	{
+		$ui.find('[type="search"]')
+			.addClass('active')
+			.addBack()
+			.find('.txp-dropdown')
+			.show();
+	});
+
+	// On mouse-leave, hide the search options and change the arrow image.
+	$ui.bind('mouseleave', function()
+	{
+		$ui.find('[type="search"]')
+			.removeClass('active')
+			.addBack()
+			.find('.txp-dropdown')
+			.hide();
+	});
+
+	$ui.txpMultiEditForm({
+		'checkbox'    : 'input[name="search_method[]"][type=checkbox]',
+		'row'         : '.txp-dropdown li',
+		'highlighted' : '.txp-dropdown li',
+		'confirmation': false
+	});
+});
