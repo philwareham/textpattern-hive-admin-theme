7a197a9bf463776f270c00c64ffea9e26fd17f5c
Phil Wareham <phil@designhive.com>
2012-12-22 15:44:47 +0000

    Non-core: further work on search widget

--- txp4-admin-templates-new/searchbox.js
+++ txp4-admin-templates-new/searchbox.js
@@ -1,8 +1,8 @@
 $(function() {
 	var $ui = $('.txp-search');
 
-	// On focus and on click, display the search options and change the arrow image.
-	$ui.find('[type="search"]').bind('focus click', function()
+	// On focus or on click, display the search options and change the arrow image.
+	$ui.find('[type="search"]').on('focus click', function()
 	{
 		$ui.find('[type="search"]')
 			.addClass('active')
@@ -12,7 +12,7 @@ $(function() {
 	});
 
 	// On mouse-leave, hide the search options and change the arrow image.
-	$ui.bind('mouseleave', function()
+	$ui.on('mouseleave', function()
 	{
 		$ui.find('[type="search"]')
 			.removeClass('active')
