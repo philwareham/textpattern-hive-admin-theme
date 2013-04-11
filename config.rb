28c7cc7f3ba82a1b445c4fb4bb33da5cffae6ae4
Phil Wareham <phil@designhive.com>
2013-04-11 18:11:01 +0100

    Starting moving project to Compass/Singularity
    Gonna remove Griddle grid system too. To be continued.

---
+++ config.rb
@@ -0,0 +1,32 @@
+# Require any additional Compass plugins here:
+
+# Singularity GS (includes Breakpoint) - https://github.com/Team-Sass/Singularity
+require "singularitygs"
+
+
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# The environment mode. Defaults to :production, can also be :development
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
