86de772f5df52302223cd06c74d7b2b1a16911fd
Phil Wareham <phil@designhive.com>
2014-11-20 15:25:44 +0000

    Amends to layout of list pages

--- docs/assets/js/design-patterns.js
+++ docs/assets/js/design-patterns.js
@@ -208,39 +208,6 @@ $(function ()
     });
 
     /**
-     * Options button.
-     */
-
-    $('.txp-button-details').button({
-        text: false,
-        icons: {
-            primary: 'ui-icon-gear'
-        }
-    });
-
-    /**
-     * List view button.
-     */
-
-    $('.txp-button-list').button({
-        text: false,
-        icons: {
-            primary: 'ui-extra-icon-list'
-        }
-    });
-
-    /**
-     * Grid view button.
-     */
-
-    $('.txp-button-grid').button({
-        text: false,
-        icons: {
-            primary: 'ui-extra-icon-grid'
-        }
-    });
-
-    /**
      * Datepicker.
      */
 
