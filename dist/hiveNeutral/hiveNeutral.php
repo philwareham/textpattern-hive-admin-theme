7697d5c02a646bd64f13c649e4856ff57142844c
Phil Wareham <phil@designhive.com>
2016-01-15 16:42:11 +0000

    Build

--- dist/hiveNeutral/hiveNeutral.php
+++ dist/hiveNeutral/hiveNeutral.php
@@ -226,10 +226,11 @@ EOS;
         global $prefs;
 
         return array(
+            'title'       => 'Hive (Flat Neutral)',
+            'description' => 'Textpattern Hive (Flat Neutral) admin theme',
+            'version'     => $prefs['version'],
             'author'      => 'Team Textpattern',
             'author_uri'  => 'http://textpattern.com/',
-            'version'     => $prefs['version'],
-            'description' => 'Textpattern Hive Theme (Neutral Grey)',
             'help'        => 'https://github.com/philwareham/txp-hive-admin-theme',
         );
     }
