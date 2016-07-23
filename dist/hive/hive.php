752dd397ad8e9d718d4238117353b1a504ee2cc9
Phil Wareham <phil@designhive.com>
2016-07-23 17:30:45 +0100

    Mockups: intentionally break the header with a long site name
    Will fix in due course.

--- dist/hive/hive.php
+++ dist/hive/hive.php
@@ -113,8 +113,10 @@ class hive_theme extends \Textpattern\Admin\Theme
             $out[] = '</ul>';
             $out[] = '</nav>';
             $out[] = graf(
-                href(gTxt('tab_view_site'), hu, array('target' => '_blank')),
-                array('class' => 'txp-view-site'));
+                href(span(htmlspecialchars($GLOBALS["prefs"]["sitename"]), array('class' => 'txp-view-site-name')), hu, array(
+                    'target' => '_blank',
+                    'title'  => gTxt('tab_view_site'),
+                )), array('class' => 'txp-view-site'));
             $out[] = graf(
                 href(gTxt('logout'), 'index.php?logout=1', ' onclick="return verify(\''.gTxt('are_you_sure').'\')"'), array('class' => 'txp-logout'));
         }
