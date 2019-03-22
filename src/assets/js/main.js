68a2d9e8a7a560c1bf8567b214b3229ea8e50319
Phil Wareham <phil@designhive.com>
2019-03-22 09:23:51 +0000

    Add some initial Dark/Light Mode detection

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,3 +3,33 @@ $(function ()
     'use strict';
     autosize($('textarea:not(.code)'));
 });
+
+const DARK = '(prefers-color-scheme: dark)'
+const LIGHT = '(prefers-color-scheme: light)'
+
+function changeWebsiteTheme(scheme) {
+   // 'dark' or 'light' string is in scheme here
+   // so the website theme can be updated
+}
+
+function detectColorScheme() {
+    if (!window.matchMedia) {
+        return
+    }
+
+    function listener({matches, media}) {
+        if (!matches) { // Not matching anymore = not interesting
+            return
+        }
+        if (media === DARK) {
+            changeWebsiteTheme('dark')
+        } else if (media === LIGHT) {
+            changeWebsiteTheme('light')
+        }
+    }
+
+    const mqDark = window.matchMedia(DARK)
+    mqDark.addListener(listener)
+    const mqLight = window.matchMedia(LIGHT)
+    mqLight.addListener(listener)
+}
