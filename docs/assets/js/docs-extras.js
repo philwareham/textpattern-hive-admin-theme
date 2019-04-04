93eaf4dd2c90b052699a75a357da0a3950153d4e
Phil Wareham <phil@designhive.com>
2019-04-04 11:42:05 +0100

    Fixes after refactor

---
+++ docs/assets/js/docs-extras.js
@@ -0,0 +1,35 @@
+(function()
+{
+    'use strict';
+
+    // Switch between Dark Mode/Light Mode manually.
+
+    var bodyClass = document.querySelector('body'),
+        lightSwitch = document.getElementById('lightswitch');
+
+    lightSwitch.addEventListener('click', function(e)
+    {
+        if (bodyClass.classList.contains('darkmode')) {
+            bodyClass.classList.remove('darkmode');
+            localStorage.setItem('prefers-color-scheme', 'light');
+        } else {
+            bodyClass.classList.add('darkmode');
+            localStorage.setItem('prefers-color-scheme', 'dark');
+        }
+
+        e.preventDefault();
+    });
+
+    // ScrollSpy.
+
+    var spy = new ScrollSpy('main', {
+        nav: '.design-patterns-menu ol > li > a',
+        className: 'is-inview'
+    });
+
+    var subSpy = new ScrollSpy('main', {
+    	nav: '.design-patterns-menu ol > li > ol > li > a',
+    	className: 'is-inview'
+    });
+
+})();
