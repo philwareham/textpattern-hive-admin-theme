a3f34f8a091ba7eb8b7288d0a11cb507452ce3b1
Phil Wareham <phil@designhive.com>
2019-05-22 11:39:23 +0100

    Remove fibers

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,6 @@ module.exports = function (grunt)
     'use strict';
 
     // Define Sass implmentation (Dart-Sass).
-    const Fiber = require('fibers');
     const sass = require('dart-sass');
 
     // Load all Grunt tasks.
@@ -156,7 +155,6 @@ module.exports = function (grunt)
         // Sass configuration.
         sass: {
             options: {
-                fiber: Fiber,
                 implementation: sass,
                 //includePaths: ['node_modules/textpattern-jquery-ui-theme/scss'],
                 outputStyle: 'expanded', // outputStyle = expanded, nested, compact or compressed.
