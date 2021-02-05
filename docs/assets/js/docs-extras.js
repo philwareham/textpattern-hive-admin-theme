282491f41c0be4ed26ebbf577c24cc6b52715438
philwareham <phil@designhive.com>
2021-02-05 16:06:21 +0000

    Shorter CSS Custom Variable names
    Saves bytes

--- docs/assets/js/docs-extras.js
+++ docs/assets/js/docs-extras.js
@@ -23,12 +23,12 @@
     // ScrollSpy.
 
     var spy = new ScrollSpy('main', {
-        nav: '.design-patterns-menu ol > li > a',
+        nav: '.design-patterns--menu ol > li > a',
         className: 'is-inview'
     });
 
     var subSpy = new ScrollSpy('main', {
-    	nav: '.design-patterns-menu ol > li > ol > li > a',
+    	nav: '.design-patterns--menu ol > li > ol > li > a',
     	className: 'is-inview'
     });
 
